8c23e6c8f6b8a4cd2eae648becb38ce1caf4804e
KuroDev89 <aurelio.dc.89@gmail.com>
2020-08-18 00:45:17 -0500

    basketball match score app almost done

---
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -0,0 +1,60 @@
+package android.example.courtcounter;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int score_a= 0;
+    int score_b= 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void point_3_a(View view){
+        score_a= score_a + 3;
+        display_a(score_a);
+    }
+
+    public void point_2_a(View view){
+        score_a= score_a + 2;
+        display_a(score_a);
+    }
+    public void point_1_a(View view){
+        score_a= score_a + 1;
+        display_a(score_a);
+    }
+    public void point_3_b(View view){
+        score_b= score_b + 3;
+        display_b(score_b);
+    }
+    public void point_2_b(View view){
+        score_b= score_b + 2;
+        display_b(score_b);
+    }
+    public void point_1_b(View view){
+        score_b= score_b + 1;
+        display_b(score_b);
+    }
+
+    public void reset(View view){
+        score_a= 0;
+        score_b= 0;
+        display_a(score_a);
+        display_b(score_b);
+    }
+
+    private void display_a(int number_a){
+        TextView quantityTextView_a= findViewById(R.id.team_a_score);
+        quantityTextView_a.setText("" + number_a);
+
+    }
+    private void display_b(int number_b){
+        TextView quantityTextView_b= findViewById(R.id.team_b_score);
+        quantityTextView_b.setText("" + number_b);
+
+    }
+}
